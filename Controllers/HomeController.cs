9e7bd6af18d60d4136eeaa95a24172b11d50d5d8
Ragavan67 <150412969+Ragavan67@users.noreply.github.com>
2024-03-20 13:42:19 +0530

    ASP

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CURD.Models;
+
+namespace CURD.Controllers
+{
+    public class HomeController : Controller
+    {
+        studentEntities student=new studentEntities();
+        public List<Student>employees=new List<Student>();
+        public ActionResult Index()
+        {
+           employees = student.student.ToList();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CURD.Models
+{
+    public class Student
+    {
+        public int empid { get; set; }
+        public string empname { get; set; }
+
+        public int salary { get; set; }
+
+    }
+}
